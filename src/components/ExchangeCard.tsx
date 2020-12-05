da6af8cd890ce049cded1b0394f0f3e96f4010af
Arsene Arnaud Ouedraogo <arsene@convergence.studio>
2020-12-05 13:18:53 +0100

    Base exchange card on dynamic data

--- src/components/ExchangeCard.tsx
+++ src/components/ExchangeCard.tsx
@@ -43,35 +43,65 @@ const useStyles = makeStyles(theme => ({
 export default function ExchangeCard() {
   const classes = useStyles();
 
+  const exchangeCardData = {
+    name: 'Kraken BTC/USD',
+    speed: 53,
+    // todo add lib to round to 1 digit
+    asks: [
+      {
+        value: 7107.3,
+        otherValue: 25.0,
+      },
+      {
+        value: 7107.2,
+        otherValue: 0.9,
+      },
+      {
+        value: 7106.9,
+        otherValue: 32,
+      },
+    ],
+    bids: [
+      {
+        value: 7104.8,
+        otherValue: 22.1,
+      },
+      {
+        value: 7105.0,
+        otherValue: 4.9,
+      },
+      {
+        value: 7105.2,
+        otherValue: 2.0,
+      },
+    ],
+  };
+
   return (
     <Card className={classes.root}>
       <CardContent>
         <span className={classes.exchangeName}>
-          Kraken BTC/USD
+          {exchangeCardData.name}
         </span>
         <Typography variant="subtitle1" component="p">
-          speed: 53ob/min
+          speed: {Math.round(exchangeCardData.speed)} ob/min
         </Typography>
-        <div className={classes.orderBookLine}>
-          <span className={classes.asks}>7107.3</span>
-          <span>25.0</span>
-        </div>
-        <div className={classes.orderBookLine}>
-          <span className={classes.asks}>7107.2</span>
-          <span>0.9</span>
-        </div>
-        <div className={classes.orderBookLine}>
-          <span className={classes.asks}>7106.9</span>
-          <span>32.0</span>
-        </div>
+        {(exchangeCardData.asks.map(item => (
+          <div className={classes.orderBookLine}>
+            <span className={classes.asks}>{item.value}</span>
+            <span>{item.otherValue}</span>
+          </div>
+        )))}
         <div className={classes.mainInfo}>
           <span className={classes.midPrice}>7015.9</span>
           <span className={classes.spread}>0.03%</span>
         </div>
-        <div className={classes.orderBookLine}>
-          <span className={classes.bids}>7104.8</span>
-          <span>22.1</span>
-        </div>
+        {(exchangeCardData.bids.map(item => (
+          <div className={classes.orderBookLine}>
+            <span className={classes.bids}>{item.value}</span>
+            <span>{item.otherValue}</span>
+          </div>
+        )))}
       </CardContent>
     </Card>
   );
